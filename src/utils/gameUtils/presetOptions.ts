736b16c197f651ed1eed303022d3b5d916c2bbef
Zarazaka Mikalai <golmtz@gmail.com>
2022-10-02 22:54:30 +0300

    hide archive preset

--- src/utils/gameUtils/presetOptions.ts
+++ src/utils/gameUtils/presetOptions.ts
@@ -1,6 +1,11 @@
 import { GamePresetT } from 'app/types/GameTypes';
 
 export const presetOptions = (presets: Omit<GamePresetT, 'settings'>[]) => {
-  const presetOpt = presets.map(item => ({ value: item.name, label: item.name }));
+  const presetOpt = presets
+    .filter(el => el.status !== 'archive')
+    .map(item => ({
+      value: item.name,
+      label: item.name,
+    }));
   return [{ value: '', label: 'Создать шаблон' }, ...presetOpt];
 };
