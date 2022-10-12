0b83d48703d1424717ed66f433a991460b0ca1e7
Andrew Gaityukevich <andrewgaity@yandex.by>
2022-10-12 18:46:10 +0300

    fix: refactoring before build

---
+++ src/app/types/SearchCoursesParamsType.ts
@@ -0,0 +1,13 @@
+export class SearchCoursesParamsType {
+  level: string = '';
+
+  title: string = '';
+
+  per_page: number = 10;
+
+  page: number = 0;
+
+  total: number = 0;
+
+  created_since: string = '';
+}
