23731e34f56c06c6f651fcea08f321dd80f0f50c
Andrew Gaityukevich <andrewgaity@yandex.by>
2022-12-27 23:19:40 +0300

    fix: error name group

--- src/components/classes-page/AddEditGroup/AddEditGroup.tsx
+++ src/components/classes-page/AddEditGroup/AddEditGroup.tsx
@@ -1,21 +1,21 @@
-import { FormControl, Grid, InputLabel, Select, TextField } from '@mui/material';
+import { FormControl, Grid, InputLabel, Select, TextField, Snackbar, Alert } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { GroupLevels } from 'app/enums/GroupLevels';
+import { GroupStatus } from 'app/enums/GroupStatus';
 import { Roles } from 'app/enums/Roles';
 import usersService from 'app/services/usersService';
 import appStore from 'app/stores/appStore';
 import groupStore from 'app/stores/groupStore';
+import { GroupStatusTypes } from 'app/types/GroupStatusTypes';
 import BasicModal from 'components/basic-modal/BasicModal';
 import Button from 'components/button/Button';
 import { HomeWorksScheduleItem } from 'components/classes-page/AddEditGroup/HomeWorksScheduleItem/HomeWorksScheduleItem';
 import Lessons from 'components/classes-page/AddEditGroup/Lessons/Lessons';
 import { observer } from 'mobx-react-lite';
 import React, { FC, useEffect, useState } from 'react';
-import { getOptionMui } from 'utils/getOption';
-import { GroupStatus } from 'app/enums/GroupStatus';
-import { GroupStatusTypes } from 'app/types/GroupStatusTypes';
 import { getMUIOptionsFromEnum } from 'utils/getMUIOptionsFromEnum';
 import { getNameFromEnum } from 'utils/getNameFromEnum';
+import { getOptionMui } from 'utils/getOption';
 
 const AddEditGroup: FC = observer(() => {
   const {
@@ -30,6 +30,8 @@ const AddEditGroup: FC = observer(() => {
     closeModal,
     editGroup,
     isDisableEditForm,
+    setErrorMessage,
+    errorMessage,
   } = groupStore;
 
   const { role, user } = appStore;
@@ -89,6 +91,7 @@ const AddEditGroup: FC = observer(() => {
   const onClose = () => {
     closeModal();
     cleanModalValues();
+    setErrorMessage(null);
   };
 
   const isFranchiseRole = role === Roles.Franchisee || role === Roles.FranchiseeAdmin;
@@ -100,161 +103,185 @@ const AddEditGroup: FC = observer(() => {
     groupStore.scheduleHomeWorks = groupStore.setEmptyScheduleHomeWorksItems(count);
   };
 
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (errorMessage) {
+      setOpen(true);
+    }
+  }, [errorMessage]);
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setErrorMessage(null);
+    setOpen(false);
+  };
+
   return (
-    <BasicModal
-      fullWidth
-      title={selectedGroup.id ? 'Редактировать группу' : 'Добавить группу'}
-      visibility={isModalOpen}
-      changeVisibility={onClose}
-    >
-      <div>Все поля обязательны</div>
-      <Grid
-        container
-        spacing={2}
-        sx={{
-          paddingTop: 2,
-        }}
+    <>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {errorMessage}
+        </Alert>
+      </Snackbar>
+
+      <BasicModal
+        fullWidth
+        title={selectedGroup.id ? 'Редактировать группу' : 'Добавить группу'}
+        visibility={isModalOpen}
+        changeVisibility={onClose}
       >
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label="Название"
-            value={modalFields.name}
-            fullWidth
-            disabled={isDisableEditForm}
-            onChange={({ currentTarget: { value } }) => (modalFields.name = value)}
-            // error={!validateSchema.fields.name.isValidSync(modalFields.name)}
-          />
-        </Grid>
-        {!isFranchiseRole && (
+        <div>Все поля обязательны</div>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            paddingTop: 2,
+          }}
+        >
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Название"
+              value={modalFields.name}
+              fullWidth
+              disabled={isDisableEditForm}
+              onChange={({ currentTarget: { value } }) => (modalFields.name = value)}
+              // error={!validateSchema.fields.name.isValidSync(modalFields.name)}
+            />
+          </Grid>
+          {!isFranchiseRole && (
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+                <InputLabel id="franchise">Франшиза</InputLabel>
+                <Select
+                  labelId="franchise"
+                  label="Франшиза"
+                  fullWidth
+                  disabled={isDisableEditForm}
+                  onChange={event => (modalFields.franchiseId = event.target.value)}
+                  value={modalFields.franchiseId}
+                >
+                  {franchiseOptions}
+                </Select>
+              </FormControl>
+            </Grid>
+          )}
           <Grid item xs={12} sm={6}>
             <FormControl fullWidth>
-              <InputLabel id="franchise">Франшиза</InputLabel>
+              <InputLabel id="level">Уровень</InputLabel>
               <Select
-                labelId="franchise"
-                label="Франшиза"
+                labelId="level"
+                label="Уровень"
+                placeholder="Уровень"
                 fullWidth
                 disabled={isDisableEditForm}
-                onChange={event => (modalFields.franchiseId = event.target.value)}
-                value={modalFields.franchiseId}
+                // @ts-ignore
+                onChange={event => (modalFields.level = event.target.value)}
+                value={modalFields.level}
               >
-                {franchiseOptions}
+                {getMUIOptionsFromEnum(GroupLevels)}
               </Select>
             </FormControl>
           </Grid>
-        )}
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth>
-            <InputLabel id="level">Уровень</InputLabel>
-            <Select
-              labelId="level"
-              label="Уровень"
-              placeholder="Уровень"
-              fullWidth
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id="teacher">Учитель</InputLabel>
+              <Select
+                labelId="teacher"
+                label="Учитель"
+                fullWidth
+                onChange={event => (modalFields.teacherId = event.target.value)}
+                value={modalFields.teacherId}
+                disabled={isDisableEditForm || isDisableTeacherSelector}
+              >
+                {teacherOptions}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id="course">{courseLabel}</InputLabel>
+              <Select
+                labelId="course"
+                label={courseLabel}
+                disabled={!modalFields.level || isDisableEditForm}
+                fullWidth
+                onChange={({ target: { value } }) => {
+                  const course = groupStore.courses.find(el => el.id === value);
+                  course && setSchedule(course.worksCount);
+                  modalFields.courseId = value;
+                }}
+                value={modalFields.courseId}
+              >
+                {courseOptions}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id="status">Статус</InputLabel>
+              <Select
+                labelId="status"
+                label="Статус"
+                fullWidth
+                onChange={event => (modalFields.status = event.target.value as GroupStatusTypes)}
+                value={modalFields.status}
+              >
+                {getMUIOptionsFromEnum(GroupStatus)}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <DatePicker
               disabled={isDisableEditForm}
-              // @ts-ignore
-              onChange={event => (modalFields.level = event.target.value)}
-              value={modalFields.level}
-            >
-              {getMUIOptionsFromEnum(GroupLevels)}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth>
-            <InputLabel id="teacher">Учитель</InputLabel>
-            <Select
-              labelId="teacher"
-              label="Учитель"
-              fullWidth
-              onChange={event => (modalFields.teacherId = event.target.value)}
-              value={modalFields.teacherId}
-              disabled={isDisableEditForm || isDisableTeacherSelector}
-            >
-              {teacherOptions}
-            </Select>
-          </FormControl>
-        </Grid>
+              onChange={e => e && (modalFields.dateSince = new Date(e))}
+              value={modalFields.dateSince}
+              renderInput={e => <TextField {...e} sx={{ width: '48%' }} />}
+              label="Период работы группы с"
+            />
+            <DatePicker
+              disabled={isDisableEditForm}
+              onChange={e => e && (modalFields.dateUntil = new Date(e))}
+              value={modalFields.dateUntil}
+              renderInput={e => <TextField {...e} sx={{ width: '48%' }} />}
+              label="по"
+            />
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth>
-            <InputLabel id="course">{courseLabel}</InputLabel>
-            <Select
-              labelId="course"
-              label={courseLabel}
-              disabled={!modalFields.level || isDisableEditForm}
-              fullWidth
-              onChange={({ target: { value } }) => {
-                const course = groupStore.courses.find(el => el.id === value);
-                course && setSchedule(course.worksCount);
-                modalFields.courseId = value;
-              }}
-              value={modalFields.courseId}
-            >
-              {courseOptions}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth>
-            <InputLabel id="status">Статус</InputLabel>
-            <Select
-              labelId="status"
-              label="Статус"
-              fullWidth
-              onChange={event => (modalFields.status = event.target.value as GroupStatusTypes)}
-              value={modalFields.status}
-            >
-              {getMUIOptionsFromEnum(GroupStatus)}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <DatePicker
-            disabled={isDisableEditForm}
-            onChange={e => e && (modalFields.dateSince = new Date(e))}
-            value={modalFields.dateSince}
-            renderInput={e => <TextField {...e} sx={{ width: '48%' }} />}
-            label="Период работы группы с"
-          />
-          <DatePicker
-            disabled={isDisableEditForm}
-            onChange={e => e && (modalFields.dateUntil = new Date(e))}
-            value={modalFields.dateUntil}
-            renderInput={e => <TextField {...e} sx={{ width: '48%' }} />}
-            label="по"
-          />
-        </Grid>
+          <Grid item>
+            <Lessons />
+          </Grid>
 
-        <Grid item>
-          <Lessons />
-        </Grid>
+          <Grid item sx={{ width: '100%' }}>
+            <HomeWorksScheduleItem />
+          </Grid>
 
-        <Grid item sx={{ width: '100%' }}>
-          <HomeWorksScheduleItem />
-        </Grid>
-
-        <Grid
-          item
-          sm={12}
-          sx={{
-            display: 'flex',
-            flexDirection: 'row-reverse',
-          }}
-        >
-          <Button
-            onClick={() =>
-              selectedGroup.id
-                ? editGroup()
-                : isFranchiseRole
-                ? addGroup(user.franchise.id)
-                : addGroup()
-            }
+          <Grid
+            item
+            sm={12}
+            sx={{
+              display: 'flex',
+              flexDirection: 'row-reverse',
+            }}
           >
-            {selectedGroup.id ? 'Изменить' : 'Добавить'}
-          </Button>
+            <Button
+              onClick={() =>
+                selectedGroup.id
+                  ? editGroup()
+                  : isFranchiseRole
+                  ? addGroup(user.franchise.id)
+                  : addGroup()
+              }
+            >
+              {selectedGroup.id ? 'Изменить' : 'Добавить'}
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </BasicModal>
+      </BasicModal>
+    </>
   );
 });
 
